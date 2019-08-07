f633254b1eb45169c523e285b813d36d2ab49d28
obscure723 <obscure723@yone-lab.com>
2019-08-07 10:36:11 +0800

    プロジェクト作成

---
+++ GoogleSignInProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSignInProject
+//
+//  Created by yonekan on 2019/08/07.
+//  Copyright © 2019 yonekan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
